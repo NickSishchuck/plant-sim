bc74176a77cf03bb5d44190bbc4ace3d18e7d6a4
NickSishchuck <mikiitasisuk@gmail.com>
2025-12-22 07:27:20 +0200

    ControlRods.insert_by() function

--- src/control_rods.rs
+++ src/control_rods.rs
@@ -1,10 +1,14 @@
 pub fn greeting() {
     println!("I'm from control_rods");
-    let rod1 = ControlRods::new(12.3);
+    let mut rod1 = ControlRods::new(12.3);
     println!(
         "Control Rods insertion persent is: {}",
         rod1.insertion_persent,
-    )
+    );
+
+    if let Err(e) = rod1.insert_by(1.7) {
+        println!("Error: {}", e);
+    }
 }
 
 struct ControlRods {
@@ -16,5 +20,19 @@ impl ControlRods {
         ControlRods {
             insertion_persent: insertion.clamp(0.0, 100.0),
         }
+    }
+    pub fn insert_by(&mut self, delta: f32) -> Result<f32, String> {
+        let new_pos = self.insertion_persent + delta;
+
+        if !(0.0..=100.0).contains(&new_pos) {
+            return Err(format!("Cannot move to {}%", new_pos));
+        }
+
+        self.insertion_persent = new_pos;
+        println!(
+            "ControlRod is moved by: {}% Final value: {}%",
+            delta, self.insertion_persent
+        );
+        Ok(self.insertion_persent)
     }
 }
